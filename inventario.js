3bccdfdc6a2f01945d44c647e510519570d28409
CarmenSRM <cramos15@ucol.mx>
2022-10-16 01:27:37 -0500

    Archivos, App, Clases de Inventario y Producto

---
+++ inventario.js
@@ -0,0 +1,18 @@
+class Inventario{
+    constructor(){
+        this._primero = null;
+    }
+
+    agregar(producto){}
+
+    buscar(codigo){}
+    
+    eliminar(codifo){}
+
+    insertar(posicion, producto){}
+    
+    listar(){}
+
+    listarInverso(){}
+
+}
